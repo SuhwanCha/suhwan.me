38d8a389fdd947f42186b5a3a6b69f5118d5aa6b
SuhwanCha <suhw4n@gmail.com>
2023-10-07 23:24:14 +0900

    fix: home screen msgs

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,18 +20,16 @@ class HomeScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.displayLarge,
               ),
               Text(
-                "I'm Suhwan Cha, a software engineer based in South Korea.",
+                "Suhwan Cha, a software engineer based in South Korea.",
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
               Text(
-                'I have a passion for software development and open source. ',
-                style: Theme.of(context).textTheme.bodyMedium,
+                'He has a passion for software development and open source.',
+                style: Theme.of(context).textTheme.bodyLarge,
               ),
-
               const SizedBox(
                 height: 20,
               ),
-
               Row(
                 children: [
                   Expanded(
@@ -75,34 +73,8 @@ class HomeScreen extends StatelessWidget {
                               Chip(
                                 label: Text('Docker/Kubernetes'),
                               ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "What I'm interested in",
-                          style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.tertiary,
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          padding: const EdgeInsets.all(8),
-                          child: const Wrap(
-                            spacing: 8,
-                            runSpacing: 8,
-                            children: [
                               Chip(
-                                label: Text('Cyclying'),
-                              ),
-                              Chip(
-                                label: Text('Classical Music'),
+                                label: Text('CI/CD'),
                               ),
                             ],
                           ),
@@ -110,58 +82,85 @@ class HomeScreen extends StatelessWidget {
                         const SizedBox(
                           height: 20,
                         ),
-                        Text(
-                          "The person I like",
-                          style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // 열정적인 사람
-                            Text(
-                              '- Passionate people',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                            // 노력하는 사람
-                            Text(
-                              '- Hardworking people',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                            // 책임감 있는 사람
-                            Text(
-                              '- Responsible people',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                            // 배려심 있는 사람
-                            Text(
-                              '- Considerate people',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                            // 끈기 있는 사람
-                            Text(
-                              '- Persistent people',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                            // 성실한 사람
-                            Text(
-                              '- Diligent people',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          ],
-                        )
+                        // // Text(
+                        // //   "What I'm interested in",
+                        // //   style: Theme.of(context).textTheme.headlineMedium,
+                        // // ),
+                        // // const SizedBox(
+                        // //   height: 10,
+                        // // ),
+                        // // Container(
+                        // //   decoration: BoxDecoration(
+                        // //     color: Theme.of(context).colorScheme.tertiary,
+                        // //     borderRadius: BorderRadius.circular(5),
+                        // //   ),
+                        // //   padding: const EdgeInsets.all(8),
+                        // //   child: const Wrap(
+                        // //     spacing: 8,
+                        // //     runSpacing: 8,
+                        // //     children: [
+                        // //       Chip(
+                        // //         label: Text('Cyclying'),
+                        // //       ),
+                        // //       Chip(
+                        // //         label: Text('Classical Music'),
+                        // //       ),
+                        // //     ],
+                        // //   ),
+                        // // ),
+                        // const SizedBox(
+                        //   height: 20,
+                        // ),
+                        // Text(
+                        //   "The person I like",
+                        //   style: Theme.of(context).textTheme.headlineMedium,
+                        // ),
+                        // const SizedBox(
+                        //   height: 10,
+                        // ),
+                        // Column(
+                        //   crossAxisAlignment: CrossAxisAlignment.start,
+                        //   children: [
+                        //     // 열정적인 사람
+                        //     Text(
+                        //       '- Passionate people',
+                        //       style: Theme.of(context).textTheme.bodyMedium,
+                        //     ),
+                        //     // 노력하는 사람
+                        //     Text(
+                        //       '- Hardworking people',
+                        //       style: Theme.of(context).textTheme.bodyMedium,
+                        //     ),
+                        //     // 책임감 있는 사람
+                        //     Text(
+                        //       '- Responsible people',
+                        //       style: Theme.of(context).textTheme.bodyMedium,
+                        //     ),
+                        //     // 배려심 있는 사람
+                        //     Text(
+                        //       '- Considerate people',
+                        //       style: Theme.of(context).textTheme.bodyMedium,
+                        //     ),
+                        //     // 끈기 있는 사람
+                        //     Text(
+                        //       '- Persistent people',
+                        //       style: Theme.of(context).textTheme.bodyMedium,
+                        //     ),
+                        //     // 성실한 사람
+                        //     Text(
+                        //       '- Diligent people',
+                        //       style: Theme.of(context).textTheme.bodyMedium,
+                        //     ),
+                        //   ],
+                        // )
                       ],
                     ),
                   )
                 ],
               ),
-
               const SizedBox(
                 height: 20,
               ),
-
               Row(
                 children: [
                   ElevatedButton(
@@ -190,7 +189,13 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              // large button
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Love each other as I have loved you. (John 15:12 NIV)',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
             ],
           ),
         ),
